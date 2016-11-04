ccb03881b173b021094689ffec82699b1ba68bb3
Michal Michalski <michal.michalski.ctr@sabre.com>
2016-11-04 22:58:03 +0100

    [cpp] problem 018 - find mamimum sum in triangle

---
+++ cpp/problem018.cpp
@@ -0,0 +1,58 @@
+/*
+    Copyright (c) 2015 - buyuk.dev@gmail.com
+    ----------------------------------------
+    << INSERT DESCRIPTION HERE >>
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) 
+{
+   int triSize = 0;
+   cin >> triSize;
+
+   vector<vector<int> > tri(triSize);
+   int cell = -1;
+   
+   for (int rowId = 0; rowId < triSize; ++rowId)
+   {
+      for (int cellId = 0; cellId <= rowId; ++cellId)
+      {
+         cin >> cell;
+         tri[rowId].push_back(cell);
+      }
+   }
+ 
+   for (int rowId = 0; rowId < triSize - 1; ++rowId)
+   {
+      vector<int> temp = tri[rowId + 1];
+      for (int cellId = 0; cellId < tri[rowId].size(); ++cellId)
+      {
+         tri[rowId + 1][cellId] = max(tri[rowId + 1][cellId], temp[cellId] + tri[rowId][cellId]);
+         tri[rowId + 1][cellId + 1] = max(tri[rowId + 1][cellId + 1], temp[cellId + 1] + tri[rowId][cellId]);
+      }
+   }
+
+   cout << "Max sum is " << *max_element(tri.back().begin(), tri.back().end()) << endl;
+   
+   return 0;
+}
+
+
+
+
+
+
+
