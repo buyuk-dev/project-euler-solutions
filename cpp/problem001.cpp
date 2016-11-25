f325d3371913d447c79f50b71478885b36b9235d
buyuk <buyuk.dev@gmail.com>
2016-11-25 15:10:37 +0100

    [cpp] problem 001 - load data from file

--- cpp/problem001.cpp
+++ cpp/problem001.cpp
@@ -6,6 +6,32 @@
 */
 
 #include <cstdio>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+struct Data
+{
+    int a, b, lim;
+};
+
+
+Data load(string filename)
+{
+    Data data;
+    
+    ifstream dataFile(filename.c_str());
+    if (!dataFile)
+    {
+        throw string("File does not exist.");
+    }
+
+    dataFile >> data.a >> data.b >> data.lim;
+   
+    return data;
+}
 
 
 int sumOfMultiplesOfN(int n, int limit)
@@ -14,14 +40,31 @@ int sumOfMultiplesOfN(int n, int limit)
    return n * (kmax * (kmax + 1) / 2);
 }
 
+
+int solve(int a, int b, int lim)
+{
+    int sumA = sumOfMultiplesOfN(a, lim - 1);
+    int sumB = sumOfMultiplesOfN(b, lim - 1);
+    int sumAB = sumOfMultiplesOfN(a * b, lim - 1);
+
+    return sumA + sumB - sumAB;
+}
+
+
 int main()
 {
-   int sum3 = sumOfMultiplesOfN(3, 999);
-   int sum5 = sumOfMultiplesOfN(5, 999);
-   int sum15 = sumOfMultiplesOfN(15, 999);
-   
-   int sum3or5 = sum3 + sum5 - sum15;
-   
-   printf("Sum of multiples of 3 or 5 below 1000 is %d\n", sum3or5);
-   return 0;
+    try 
+    {
+        Data data = load("data/problem001.dat");
+
+        int result = solve(data.a, data.b, data.lim);
+
+        printf("Sum of multiples of %d or %d below %d is %d\n",
+               data.a, data.b, data.lim, result);
+    }
+    catch (string err)
+    {
+        fprintf(stderr, "[ ERROR ] %s.\n", err.c_str());
+    }
+    return 0;
 }
